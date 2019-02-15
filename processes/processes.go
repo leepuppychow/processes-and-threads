489125b0fb137b83a72bd459346eac6c9f32ff9e
leepuppychow <leepuppychow@gmail.com>
2019-02-15 12:26:19 -0700

    Able to generate a list of Processes now

--- processes/processes.go
+++ processes/processes.go
@@ -1,43 +1,58 @@
 package processes
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
 type Process struct {
 	ProcessId   int
 	ThreadCount int
-	Children    []*Process
+	Children    map[string]*Process
 	Command     string
 	Duration    string
 }
 
 func ExecuteCommand(command string, args ...string) {
-	cmd := exec.Command(command, args...)
-	stdout, err := cmd.StdoutPipe()
+	output, err := exec.Command(command, args...).Output()
 	if err != nil {
 		log.Fatal(err)
 	}
-	go PrintOutput(stdout)
-	if err := cmd.Start(); err != nil {
-		log.Fatal(err)
-	}
-	if err := cmd.Wait(); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(string(output))
+	CreateProcessList(string(output))
 }
 
-func PrintOutput(stdout io.ReadCloser) {
-	scanner := bufio.NewScanner(stdout)
-	for scanner.Scan() {
-		// Here map each line of output to a Process
-		// Also get child processes for each process
-		fmt.Println(scanner.Text())
-		fmt.Println(strings.Fields(scanner.Text()))
+func CreateProcessList(stdout string) []Process {
+	allProcs := []Process{}
+	stringLines := strings.Split(strings.TrimSpace(stdout), "\n")
+	for i, line := range stringLines {
+		if i == 0 {
+			continue
+		}
+		lineSlice := strings.Fields(line)
+		id, err := strconv.Atoi(lineSlice[1])
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		threadCount, err := strconv.Atoi(lineSlice[8])
+		if err != nil {
+			fmt.Println(err)
+			threadCount = 0
+		}
+
+		p := Process{
+			ProcessId:   id,
+			ThreadCount: threadCount,
+			Children:    nil,
+			Command:     lineSlice[7],
+			Duration:    lineSlice[6],
+		}
+		allProcs = append(allProcs, p)
 	}
+
+	return allProcs
 }
